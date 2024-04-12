4cec86561d6d54e7120ee121ad88df8800b6893b
francescoberr <166726424+francescoberr@users.noreply.github.com>
2024-04-12 09:10:44 +0200

    Changed the view to work with tabs

--- view.py
+++ view.py
@@ -7,22 +7,21 @@ from model import Habit
 class HabitView(tkinter.Tk):
     def __init__(self):
         super().__init__()
+
         self.title("Habit Tracking App")
-        self.geometry("600x500")
 
-        frame = tkinter.Frame(self)
-        frame.pack()
+        self.tab_control = ttk.Notebook(self)
+        self.tab_control.pack(expand=1, fill="both")
+        
+        # --- Start Habit Creation Frame ---
 
-        # --- Start Habit Creation Outer Frame ---
+        habit_creation_frame = ttk.Frame(self.tab_control)
+        self.tab_control.add(habit_creation_frame, text="Create Habit")
 
-        habit_creation_frame = tkinter.LabelFrame(frame, text="Here you can create an habit", borderwidth=5)
-        habit_creation_frame.grid(row=0, column=0, padx=20, pady=10)
-
-        new_habit_label = tkinter.Label(habit_creation_frame, text="Create your habit:", font='Helvetica 12 bold')
+        new_habit_label = ttk.Label(habit_creation_frame, text="Create your habit:", font='Helvetica 12 bold')
         new_habit_label.grid(row=0, column=0)
         predefined_habit_label = tkinter.Label(habit_creation_frame, text="Or choose a predefined one:", font='Helvetica 12 bold')
         predefined_habit_label.grid(row=0, column=1)
-
         new_habit_entry = tkinter.Entry(habit_creation_frame)
         new_habit_entry.grid(row=1, column=0)
 
@@ -48,14 +47,14 @@ class HabitView(tkinter.Tk):
         create_button = tkinter.Button(habit_creation_frame, text="CREATE")
         create_button.grid(row=3, column=0, columnspan=2, pady=5)
 
-        # --- End Habit Creation Outer Frame ---
+        # --- End Habit Creation Outer Frame --
 
         # --- Start Habit Tracking Frame ---
 
-        habit_tracking_frame = tkinter.LabelFrame(frame, text="Here you can keep track of your habits", borderwidth=5)
-        habit_tracking_frame.grid(row=1, column=0, padx=20, pady=10, sticky="news", columnspan=2)
+        habit_tracking_frame = ttk.Frame(self.tab_control)
+        self.tab_control.add(habit_tracking_frame, text="Track Habit")
 
-        tracking_habit_label = tkinter.Label(habit_tracking_frame, text="Name", font='Helvetica 12 bold')
+        tracking_habit_label = ttk.Label(habit_tracking_frame, text="Name", font='Helvetica 12 bold')
         tracking_habit_label.grid(row=0, column=0)
         tracking_habit_entry = tkinter.Entry(habit_tracking_frame)
         tracking_habit_entry.grid(row=1, column=0, pady=5)
@@ -79,12 +78,12 @@ class HabitView(tkinter.Tk):
 
         # --- End Habit Tracking Frame ---
 
-        # --- Start Drop an Habit Frame ---
+        # --- Start Habit Drop Frame ---
 
-        habit_drop_frame = tkinter.LabelFrame(frame, text="Here you can drop an habit", borderwidth=5)
-        habit_drop_frame.grid(row=2, column=0, padx=20, pady=10, sticky="news", columnspan=2)
-        
-        drop_name_habit_label = tkinter.Label(habit_drop_frame, text="Name", font='Helvetica 12 bold')
+        habit_drop_frame = ttk.Frame(self.tab_control)
+        self.tab_control.add(habit_drop_frame, text="Drop Habit")
+
+        drop_name_habit_label = ttk.Label(habit_drop_frame, text="Name", font='Helvetica 12 bold')
         drop_name_habit_label.grid(row=0, column=0)
         drop_name_habit_entry = tkinter.Entry(habit_drop_frame)
         drop_name_habit_entry.grid(row=1, column=0, pady=5)
@@ -97,14 +96,14 @@ class HabitView(tkinter.Tk):
         for widget in habit_drop_frame.winfo_children():
             widget.grid_configure(padx=35)
 
-        # --- End Drop an Habit Frame ---
-
+        # --- End Habit Drop Frame
+            
         # --- Start Habit Analysis Frame ---
+            
+        habit_analysis_frame = ttk.Frame(self.tab_control)
+        self.tab_control.add(habit_analysis_frame, text="Analyze Habit")
 
-        habit_analysis_frame = tkinter.LabelFrame(frame, text="Here you can analyze your habits", borderwidth=5)
-        habit_analysis_frame.grid(row=3, column=0, padx=20, pady=10, sticky="news", columnspan=2)
-
-        currently_tracked_button = tkinter.Button(habit_analysis_frame, text="Currently Tracked Habits")
+        currently_tracked_button = ttk.Button(habit_analysis_frame, text="Currently Tracked Habits")
         currently_tracked_button.grid(row=0, column=0)
 
         same_periodicity_button = tkinter.Button(habit_analysis_frame, text="Same periodicity Habits")
