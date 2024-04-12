fc4ea72a84535a3695192d9d771ffc2c49c12bd0
francescoberr <166726424+francescoberr@users.noreply.github.com>
2024-04-12 16:44:38 +0200

    Prototype for Database and its methods

--- model.py
+++ model.py
@@ -8,3 +8,27 @@ class Habit:
         self.habit_type = habit_type
         self.date = date or datetime.now()  # Current date if it's not provided
         self.completed = completed
+
+class HabitDataBase:
+    def __init__(self, db_file):
+        self.conn = sqlite3.connect(db_file)
+        self.cursor = self.conn.cursor()
+        self.create_table()
+
+    def create_table(self):
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS Habits
+                               (id INTEGER PRIMARY KEY,
+                                name TEXT NOT NULL,
+                                habit_type TEXT,
+                                date INTEGER,
+                                completed INTEGER)''')
+        self.conn.commit()
+
+    def insert_habit(self, habit):
+        self.cursor.execute('''INSERT INTO Habits (name, habit_type, date, completed)
+                               VALUES (?, ?, ?, ?)''', (habit.name, habit.habit_type, habit.date, habit.completed))
+        self.conn.commit()
+
+    def close(self):
+        self.conn.close()
+    
